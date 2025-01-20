020c8e6f242c3acfb243fa48d42345b34a4debe8
mbspl <156318647+mbspl-office@users.noreply.github.com>
2025-01-20 12:05:22 +0530

    Update api.py

--- api.py
+++ api.py
@@ -9,6 +9,32 @@ from docx import Document
 from docx.shared import Inches
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+html = f"""
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>FastAPI on Vercel</title>
+        <link rel="icon" href="/static/favicon.ico" type="image/x-icon" />
+    </head>
+    <body>
+        <div class="bg-gray-200 p-4 rounded-lg shadow-lg">
+            <h1>Hello from FastAPI@{__version__}</h1>
+            <ul>
+                <li><a href="/docs">/docs</a></li>
+                <li><a href="/redoc">/redoc</a></li>
+            </ul>
+            <p>Powered by <a href="https://vercel.com" target="_blank">Vercel</a></p>
+        </div>
+    </body>
+</html>
+"""
+
+@app.get("/")
+async def root():
+    return HTMLResponse(html)
+
 
 # Enable CORS for development purposes
 app.add_middleware(
